72b2f40e08f61b268b3d990a0e51ddb2df62f3b7
rogodar <rogodar@gmail.com>
2019-01-31 10:11:01 +0200

    office change

---
+++ coursera/PythonAWD/py4e_12_3.py
@@ -0,0 +1,13 @@
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+
+counts = dict()
+for line in fhand:
+    words = line.decode().split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+print(counts)
+# prints items in dictionary line by line without "{} or ,"
+for y,x in counts.items():
+    print(y,x)
