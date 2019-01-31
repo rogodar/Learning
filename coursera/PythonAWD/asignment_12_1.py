91a49a477e6a93de14c94acde28eb8eb41256558
rogodar <rogodar@gmail.com>
2019-01-31 12:58:57 +0200

    office work

---
+++ coursera/PythonAWD/asignment_12_1.py
@@ -0,0 +1,30 @@
+# Scraping Numbers from HTML using BeautifulSoup In this assignment
+#  you will write a Python program similar to http://www.py4e.com/code3/urllink2.py. 
+# The program will use urllib to read the HTML from the data files below, and parse the data, 
+# extracting numbers and compute the sum of the numbers in the file.
+
+# Data Format
+# The file is a table of names and comment counts. You can ignore most of the data in the file except for lines like the following:
+# <tr><td>Modu</td><td><span class="comments">90</span></td></tr>
+# <tr><td>Kenzie</td><td><span class="comments">88</span></td></tr>
+# <tr><td>Hubert</td><td><span class="comments">87</span></td></tr>
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+
+    # url = "http://py4e-data.dr-chuck.net/comments_42.html"
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('span')
+# for tag in tags :
+#     print(tag.get('class', ))
+print(tags)
