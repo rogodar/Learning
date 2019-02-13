14b66e8551e4817ade7a05508a1bc2f8108ac0e6
rogodar <rogodar@gmail.com>
2019-02-13 14:19:38 +0200

    home change

--- coursera/PythonAWD/test.py
+++ coursera/PythonAWD/test.py
@@ -1,18 +1,6 @@
-import urllib.request, urllib.parse, urllib.error
-import xml.etree.ElementTree as ET 
-
-url = input('Enter adress: ')
-print('Processing :', url)
-
-x = 0
-code = urllib.request.urlopen(url).read()
-print('Retrieved: ', len(code))
-tree = ET.fromstring(code)
-count = tree.findall('.//name')
-print('Number of numbers: ', len(count))
-
-for counts in count:
-    element = counts.text
-    print(element)
-    # x += int(element)
-# print(x)
+import json
+json = json.loads(open('C:\Learning\Learning\coursera\PythonAWD\\test.json').read())
+for item in json:
+    print('Name: ', item['name'])
+    print('Id: ', item['position'])
+    print('*'*20)
