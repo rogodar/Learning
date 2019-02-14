b66e361a2aa494631cef5cc54a110876b31f2706
rogodar <rogodar@gmail.com>
2019-02-14 12:02:17 +0200

    office change

--- coursera/PythonDB/py4e_15_1.py
+++ coursera/PythonDB/py4e_15_1.py
@@ -6,24 +6,27 @@ cur = conn.cursor()
 cur.execute('DROP TABLE IF EXISTS Counts')
 
 cur.execute('''
-CREATE TABLE Counts (email TEXT, count INTEGER)''')
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
 
 fname = input('Enter file name: ')
-if (len(fname) < 1): fname = 'D:\WWW\Learning\coursera\PythonDB\mbox-short.txt'
+if (len(fname) < 1): fname = 'E:\Learning\WEB\Learning\coursera\PythonDB\mbox-short.txt'
 fh = open(fname)
 for line in fh:
     if not line.startswith('From: '): continue
     pieces = line.split()
     email = pieces[1]
-    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email,))
+    domain = email.find('@')
+    org = email[domain+1:len(email)]
+
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
     row = cur.fetchone()
     if row is None:
-        cur.execute('''INSERT INTO Counts (email, count) VALUES (?, 1)''', (email,))
+        cur.execute('''INSERT INTO Counts (org, count) VALUES (?, 1)''', (org,))
     else:
-        cur.execute('UPDATE Counts SET count = count + 1 WHERE email= ?',(email,))
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org= ?',(org,))
     conn.commit()
 # https://www.sqlite.org/lang_select.html
-sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
 for row in cur.execute(sqlstr):
     print(str(row[0]), row[1])
     
