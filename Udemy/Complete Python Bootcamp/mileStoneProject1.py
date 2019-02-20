da72be2cdef85b120b559c89375ed7dc5969f600
rogodar <rogodar@gmail.com>
2019-02-20 13:46:14 +0200

    office change

--- Udemy/Complete Python Bootcamp/mileStoneProject1.py
+++ Udemy/Complete Python Bootcamp/mileStoneProject1.py
@@ -3,6 +3,8 @@ from IPython.display import clear_output
 # step 1 - draw the board
 # step 2 - ask player if he is playing X or O
 # step 3 - separate board on 9 numbered fields
+# add player info for mark that is playing
+# add player names input ?
 # step 4 - ask player for number 1-9 according to num fields
 # reset board view after each player move
 # make rulls for player wins
@@ -17,10 +19,11 @@ def display_board(board):
     print('|----|-----|----|')
     print('| '+board[1]+'  |  '+board[2]+'  |  '+board[3]+' |')
     print('|---------------|')
+    return(player1, player2)
 
 
-test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O','X']
-display_board(test_board)
+test_board = [' ']*10
+# display_board(test_board)
 
 def player_input():
 
@@ -32,12 +35,12 @@ def player_input():
         marker = input('Player 1, choose X or O: ')
     
     # Assign Player 2, the oposite marker
-    player1 = marker
-
     if player1 == 'X':
         player2 = 'O'
     else:
         player2 = 'X'
+    
     return (player1, player2)
 
-# ------------------------
+player_input()
+display_board(test_board)
